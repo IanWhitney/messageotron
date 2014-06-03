31bc0c20adba49fdecb0a7b5aea0b1d53c6c87ec
Ian Whitney <whit0694@umn.edu>
2014-06-02 19:13:32 -0500

    Step 5: Posts Index Page
    Details:
    http://docs.railsbridge.org/intermediate-rails/make_a_posts_index_page
    
    Steps:
    
    - Added index view to routes and controller.
    - Created index.html.erb file and _post.html.erb partial
    - Added two helper methods to post.rb to keep views simple
    - Used the new helper methods in the Show template as well.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,12 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :title, :content
+
+  def author
+    user.email
+  end
+
+  def posted_on
+    created_at.to_formatted_s(:long)
+  end
 end
